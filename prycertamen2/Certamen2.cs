59ff37a14f1cc0fa2d6b951de01e5063623e918d
gama1198 <108492862+gama1198@users.noreply.github.com>
2022-07-16 16:25:41 -0400

    Primer Commit

---
+++ prycertamen2/Certamen2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prycertamen2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb1_TextChanged(object sender, EventArgs e)
+        {
+            string nombre = txb1.Text;
+          
+           
+        }
+
+        private void lbl7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb2_TextChanged(object sender, EventArgs e)
+        {
+         
+        }
+        int contador = 0;
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            if (txb2.Text.Length < 9 || txb2.Text.Length > 9)
+            {
+                MessageBox.Show("Debe tener 9 digitos", "Error");
+            }
+            else
+            {
+
+
+                if (ltb1.Items.Count <= 6)
+                {
+
+
+                    string nombre = (txb1.Text).ToString();
+                    int telefono = int.Parse(txb2.Text);
+                    string usuario = ("Nombre: " + nombre + " Teléfono: " + telefono);
+                    ltb1.Items.Add(usuario);
+
+                    contador++;
+                    lblcontador.Text = contador.ToString();
+
+
+                    nombre = txb1.Text;
+                    MessageBox.Show("Has agregado a  " + nombre);
+
+                    txb1.Clear();
+                    txb2.Clear();
+
+                }
+                else
+                {
+                    MessageBox.Show("Limite de contactos", "ERROR");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            if (ltb1.SelectedItems.Count >0)
+
+            {
+
+
+                ltb1.Items.Remove(ltb1.Text);
+                contador--;
+                lblcontador.Text = contador.ToString();
+                MessageBox.Show("Se ha eliminado un contacto", "ERROR");
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un contacto", "ERROR");
+            }
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ prycertamen2/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace prycertamen2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.lblnumero = new System.Windows.Forms.Label();
+            this.txb1 = new System.Windows.Forms.TextBox();
+            this.txb2 = new System.Windows.Forms.TextBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.lsb1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(39, 57);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(58, 17);
+            this.lblnombre.TabIndex = 0;
+            this.lblnombre.Text = "Nombre";
+            this.lblnombre.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblnumero
+            // 
+            this.lblnumero.AutoSize = true;
+            this.lblnumero.Location = new System.Drawing.Point(318, 57);
+            this.lblnumero.Name = "lblnumero";
+            this.lblnumero.Size = new System.Drawing.Size(58, 17);
+            this.lblnumero.TabIndex = 1;
+            this.lblnumero.Text = "Numero";
+            // 
+            // txb1
+            // 
+            this.txb1.Location = new System.Drawing.Point(126, 54);
+            this.txb1.Name = "txb1";
+            this.txb1.Size = new System.Drawing.Size(155, 22);
+            this.txb1.TabIndex = 2;
+            // 
+            // txb2
+            // 
+            this.txb2.Location = new System.Drawing.Point(403, 54);
+            this.txb2.Name = "txb2";
+            this.txb2.Size = new System.Drawing.Size(137, 22);
+            this.txb2.TabIndex = 3;
+            // 
+            // btningresar
+            // 
+            this.btningresar.Location = new System.Drawing.Point(571, 41);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(106, 49);
+            this.btningresar.TabIndex = 4;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            // 
+            // lsb1
+            // 
+            this.lsb1.FormattingEnabled = true;
+            this.lsb1.ItemHeight = 16;
+            this.lsb1.Location = new System.Drawing.Point(144, 114);
+            this.lsb1.Name = "lsb1";
+            this.lsb1.Size = new System.Drawing.Size(326, 292);
+            this.lsb1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 444);
+            this.Controls.Add(this.lsb1);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.txb2);
+            this.Controls.Add(this.txb1);
+            this.Controls.Add(this.lblnumero);
+            this.Controls.Add(this.lblnombre);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Agenda Telefónica";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.Label lblnumero;
+        private System.Windows.Forms.TextBox txb1;
+        private System.Windows.Forms.TextBox txb2;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.ListBox lsb1;
+    }
+}
+
